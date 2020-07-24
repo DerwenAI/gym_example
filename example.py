ba271a6fc40f7be703ca8594e10e3d8b512e034f
Paco Nathan <ceteri@gmail.com>
2020-07-23 23:08:47 -0700

    calculate baseline reward for random actions

--- example.py
+++ example.py
@@ -5,24 +5,49 @@ import gym
 import gym_example
 
 
-def main ():
-    env = gym.make("example-v0")
+def run_one_episode (env, verbose=False):
     env.reset()
     sum_reward = 0
 
     for i in range(10):
         action = env.action_space.sample()
-        print("action:", action)
+
+        if verbose:
+            print("action:", action)
 
         state, reward, done, info = env.step(action)
         sum_reward += reward
-        env.render()
+
+        if verbose:
+            env.render()
 
         if done:
-            print("done @ step {}".format(i))
+            if verbose:
+                print("done @ step {}".format(i))
+
             break
 
-    print("cumulative reward", sum_reward)
+    if verbose:
+        print("cumulative reward", sum_reward)
+
+    return sum_reward
+
+
+def main ():
+    # first, create the custom environment and run it for one episode
+    env = gym.make("example-v0")
+    sum_reward = run_one_episode(env, verbose=True)
+
+    # next, calculate a baseline of rewards based on random actions
+    # (no policy)
+    history = []
+
+    for _ in range(10000):
+        sum_reward = run_one_episode(env, verbose=False)
+        history.append(sum_reward)
+
+    avg_sum_reward = sum(history) / len(history)
+    print("\nbaseline cumulative reward: {:6.2}".format(avg_sum_reward))
 
 
 if __name__ == "__main__":
